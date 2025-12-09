f9a12d2867992659547cba46519f593295f11b2a
김택진 <qlcdmlrlf@naver.com>
2025-12-10 03:33:25 +0900

    설정

---
+++ pronted/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <h3 className='text-3xl font-bold underline text-red-500'>
+        Hello world!
+      </h3>
+    </>
+  )
+}
+
+export default App
