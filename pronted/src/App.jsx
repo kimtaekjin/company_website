6a9acdb0241ea4c04e89b9afc67ec5d53f23ac63
김택진 <qlcdmlrlf@naver.com>
2025-12-11 06:53:36 +0900

    하단 바 컴포넌트 생성

--- pronted/src/App.jsx
+++ pronted/src/App.jsx
@@ -2,12 +2,14 @@ import "./index.css";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Footer />
       </BrowserRouter>
     </>
   );
