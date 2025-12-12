85ac1fd3d50f17eb8dbadde52e158f0a7c082892
김택진 <qlcdmlrlf@naver.com>
2025-12-13 03:06:44 +0900

    1-6 app 라우팅 경로 설정

--- pronted/src/App.jsx
+++ pronted/src/App.jsx
@@ -1,18 +1,62 @@
 import "./index.css";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
-import { BrowserRouter } from "react-router-dom";
-import Footer from "./Components/Footer/Footer";
+// import { BrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-function App() {
+import Footer from "./Components/Footer/Footer";
+import MainPage from "./Page/MainPage/MainPage";
+import About from "./Page/About/About";
+import Leadership from "./Page/Leadership/Leadership";
+import Board from "./Page/Board/Board";
+import Service from "./Page/Service/Service";
+import Contact from "./Page/Contact/Contact";
+
+function Layout() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Footer />
-      </BrowserRouter>
+      <Navbar />
+      <Outlet />
+      <Footer />
     </>
   );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/our-services",
+        element: <Service />,
+      },
+      {
+        path: "/content",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ pronted/src/Page/MainPage/MainPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainPage = () => {
+  return <div className="py-32">MainPage</div>;
+};
+
+export default MainPage;
