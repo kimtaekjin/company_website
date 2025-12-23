f620bb9fc322809588dd0e95aaa0150a15bfaf86
김택진 <qlcdmlrlf@naver.com>
2025-12-23 10:00:27 +0900

    7-4 , 7-5 AWS_S3 세팅 및 routes/upload.js 생성

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,51 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const cors=require("cors");
+const cors = require("cors");
 const app = express();
-const PORT=3000;
+const PORT = 3000;
 
 const userRoutes = require("./routes/user");
 const contaactRoutes = require("./routes/contact");
 const postRoutes = require("./routes/post");
+const uploadRoutes = require("./routes/upload");
 
-app.use(cors({
+app.use(
+  cors({
     origin: "http://localhost:5173",
     credentials: true,
-}));
+  })
+);
 
-app.use(express.json())       
-app.use(express.urlencoded())   
+app.use(express.json());
+app.use(express.urlencoded());
 /* 'username' of 'req.body' as it is undefined. 에러 수정 <-뜰 시 사용
     json 파일을 익스프레스 서버에서 해석하는 기능
 */
 app.use(cookieParser()); //express에서 cookie를 읽게 함.
 
+app.use("/api/auth", userRoutes);
 
+app.use("/api/contact", contaactRoutes);
 
-app.use("/api/auth",userRoutes);
+app.use("/api/post", postRoutes);
 
-app.use("/api/contact",contaactRoutes);
+app.use("/api/upload", uploadRoutes);
 
-app.use("/api/post",postRoutes);
+app.get("/", (req, res) => {
+  app.send("Hello world");
+});
 
-
-app.get("/",(req,res)=>{
-    app.send("Hello world");
-})
-
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("MongoDB연결이 성공하였습니다");
-}).catch((error)=>{
-    console.log("MongoDB연결이 실패하였습니다",error);
-})
+  })
+  .catch((error) => {
+    console.log("MongoDB연결이 실패하였습니다", error);
+  });
 
-
-app.listen(PORT,()=>{
-    console.log("server is running");
-})
+app.listen(PORT, () => {
+  console.log("server is running");
+});
